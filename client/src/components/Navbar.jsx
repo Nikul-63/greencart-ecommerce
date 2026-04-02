6d9bd56dac8e8d62a454508aa4ffda3198387971
Nikul <dabhinikul625@gmail.com>
2026-04-02 18:36:47 +0530

    feat: implement Cart, MyOrders, and AddAddress pages with context integration

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -57,10 +57,19 @@ const Navbar = () => {
                 }
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+            <div className="flex items-center gap-6 sm:hidden">
+                <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className="w-6 opacity-80"/>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu">
                 {/* Menu Icon SVG */}
                 <img src={assets.menu_icon} alt="menu"/>
-            </button>
+                </button>
+            </div>
+
+            
 
             {/* Mobile Menu */}
             {open && (<div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
